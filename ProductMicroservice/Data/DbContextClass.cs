af81f0cba325ad7c94d3cf273f2f83747b9adf6e
Saulo Costa <saulomlcosta10@gmail.com>
2023-07-15 14:14:07 -0300

    First MService, Product.

---
+++ ProductMicroservice/Data/DbContextClass.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductMicroservice.Model;
+
+namespace ProductMicroservice.Data
+{
+    public class DbContextClass : DbContext
+    {
+        protected readonly IConfiguration configuration;
+
+        public DbContextClass(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
