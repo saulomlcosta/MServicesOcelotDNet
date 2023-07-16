39d5c961552382e9a80bd7b403199441f8cf4376
Saulo Costa <saulomlcosta10@gmail.com>
2023-07-16 15:40:06 -0300

    UserService and Migrations

---
+++ UserMicroservice/Data/DbContextClass.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using UserMicroservice.Model;
+
+namespace UserMicroservice.Data
+{
+    public class DbContextClass : DbContext
+    {
+        protected readonly IConfiguration configuration;
+
+        public DbContextClass(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
--- UserMicroservice/Program.cs
+++ UserMicroservice/Program.cs
@@ -1,7 +1,12 @@
+using UserMicroservice.Services.Interface;
+using UserMicroservice.Services;
+using UserMicroservice.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddDbContext<DbContextClass>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
